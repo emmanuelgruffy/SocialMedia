6bf6e27ade7e513c9868a0f6a626feb086ae945d
User <emmanuel.g@gloat.com>
2021-04-06 15:42:59 +0300

    db config

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI'); //see how we use the config variable -> importing it into a 'db' const.
+
+const connectDB = async () => {
+   try {
+    await mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true});
+    console.log('MongoDB Connected...');
+   } catch(err) {
+    console.log(err.message);
+    //exit process with failure:
+    process.exit(1);
+   }
+}
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const connectDB = require('./config/db'); // we are importing the db connection
 
 const app = express();
+
+//connecting the data-base:
+connectDB();
 
 const PORT = (process.env.PORT || 5000);
 
 app.listen(PORT, () => console.log('server started on port ' + PORT));
 
 app.get("/", (req, res) => res.send('Api running'));
+
+
+//see the config folder - that allow us to define config variables, this is enabled due to the npm config installation.
 
