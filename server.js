fa5287116c2e43dd330eebb47dc9e93d5876de7f
User <emmanuel.g@gloat.com>
2021-04-26 11:23:40 +0300

    setting production var

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const path = require('path');
 //see the config folder - that allow us to define config variables, this is enabled due to the npm config installation.
 const connectDB = require('./config/db'); // we are importing the db connection
 
@@ -11,13 +11,20 @@ connectDB();
 //Init Middleware - bodyparser is now part of express:
 app.use(express.json( {extended: false} ));
 
-app.get("/", (req, res) => res.send('Api running'));
-
 //Define Routes: (i.e use the routes inside the api folder)
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const PORT = (process.env.PORT || 5000);
 
